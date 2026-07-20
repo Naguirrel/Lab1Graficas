3d4c45784289a973d7b930b5dc3d31ab2b43cfc7
Norman <agu24479@uvg.edu.gt>
2026-07-19 23:24:48 -0600

    Poligono 4 and 5 added

---
+++ src/p45.rs
@@ -0,0 +1,45 @@
+use image::{Rgb, RgbImage};
+
+use crate::fill::scanline_fill;
+use crate::line::draw_polygon_border;
+
+// Poligono 4
+pub fn points() -> Vec<(i32, i32)> {
+    vec![
+        (413, 177),
+        (448, 159),
+        (502, 88),
+        (553, 53),
+        (535, 36),
+        (676, 37),
+        (660, 52),
+        (750, 145),
+        (761, 179),
+        (672, 192),
+        (659, 214),
+        (615, 214),
+        (632, 230),
+        (580, 230),
+        (597, 215),
+        (552, 214),
+        (517, 144),
+        (466, 180),
+    ]
+}
+
+// Poligono 5
+fn hole_points() -> Vec<(i32, i32)> {
+    vec![(682, 175), (708, 120), (735, 148), (739, 170)]
+}
+
+pub fn draw(img: &mut RgbImage) {
+    let fill_color = Rgb([80, 220, 120]);
+    let border_color = fill_color;
+
+    let polygon = points();
+    let hole = hole_points();
+
+    scanline_fill(img, &polygon, fill_color, &[&hole]);
+    draw_polygon_border(img, &polygon, border_color);
+    draw_polygon_border(img, &hole, border_color);
+}
